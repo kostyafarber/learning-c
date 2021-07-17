1025ef6502c1313a5c63ab90884a3fe095bfe099
Kostya Farber <kostya.farber@gmail.com>
2021-07-17 15:43:16 +1000

    stuff

--- source/tempCodeRunnerFile.c
+++ source/tempCodeRunnerFile.c
@@ -1,1 +1,18 @@
-int
+#include <stdio.h>
+
+int x = 5;
+
+int *px = NULL;
+
+*px = &x;
+
+int main(void){
+
+    printf("x\'s value is: %i\n", x);
+    printf("x\'s address is according to the pointer is: %p\n", px);
+    printf("x\'s address is: %p\n", &x);
+    printf("pointer *px\'s address is: %p\n", (void *) &px);
+    printf("The value *px is pointing to is: %i\n", *px);
+
+    return 0;
+}
