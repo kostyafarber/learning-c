073621f56c322692dbc5e5fa4be96a9c53eb2626
Kostya Farber <kostya.farber@gmail.com>
2021-07-18 18:20:49 +1000

    add gitignore and remove files

--- source/pointerArithmetic.c
+++ source/pointerArithmetic.c
@@ -8,9 +8,13 @@ int main(void){
 
     int *px = &x;
 
+
     printf("x\'s value is: %i\n", x);
-    printf("x\'s address is according to the pointer is: %p\n", px);
     printf("x\'s address is: %p\n", &x);
+
+    *px += 25; // add 25 via pointer
+
+    printf("x\'s address is according to the pointer after addition is: %p\n", px);
     printf("pointer *px\'s address is: %p\n", (void *) &px);
     printf("The value *px is pointing to is: %i\n", *px);
 
